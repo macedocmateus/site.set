7814b27bb44d05dac4d894d6ffb49d68dd354e7d
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-05-16 16:02:52 -0300

    add header

--- src/pages/blog/posts/[...id].tsx
+++
@@ -1,8 +0,0 @@
-export default function Blog() {
-  return (
-    <div>
-      <h1>post 1</h1>
-      <h2>descrição</h2>
-    </div>
-  )
-}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,5 @@
 import { Button } from '@/components/ui/button'
 
 export default function Home() {
-  return (
-    <div>
-      <h1 className="text-red-500">Hello World</h1>
-
-      <Button variant={'secondary'}>Button</Button>
-    </div>
-  )
+  return <div></div>
 }
