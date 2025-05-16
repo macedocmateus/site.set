bef29dfc6fc8bda1d954bc3b2a29bd5ebf7af775
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-05-16 14:49:09 -0300

    init

---
+++ src/pages/blog/posts/[...id].tsx
@@ -0,0 +1,8 @@
+export default function Blog() {
+  return (
+    <div>
+      <h1>post 1</h1>
+      <h2>descrição</h2>
+    </div>
+  )
+}
---
+++ src/pages/blog/posts/index.tsx
@@ -0,0 +1,3 @@
+export default function Blog() {
+  return <h1>posts</h1>
+}
