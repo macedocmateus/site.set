2ecbff082276272249a68a0e30fe92fcbf324c2a
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-06-06 16:35:09 -0300

    add primeiro post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import { LadingPage } from '@/templates/landing-page'
 
+import { allPosts } from 'contentlayer/generated'
+
 export default function Home() {
+  console.log(allPosts)
+
   return <LadingPage />
 }
