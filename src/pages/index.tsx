b211be67bbd502ad896a91717ed30a206c790acf
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-06-07 16:06:09 -0300

    initial blog page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,5 @@
 import { LadingPage } from '@/templates/landing-page'
 
-import { allPosts } from 'contentlayer/generated'
-
 export default function Home() {
-  console.log(allPosts)
-
   return <LadingPage />
 }
