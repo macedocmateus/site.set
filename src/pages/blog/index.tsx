b07f0659b23712ce1fb42df918e450d4f19095b9
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-06-08 21:27:03 -0300

    add search component

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,3 +1,5 @@
+import { Search } from '@/components/search'
+
 export default function Blog() {
   return (
     <div className="flex flex-col py-24 flex-grow h-full">
@@ -11,6 +13,7 @@ export default function Blog() {
               Dicas e estratégias para impulsionar seu negócio
             </h1>
           </div>
+          <Search />
         </div>
       </header>
     </div>
