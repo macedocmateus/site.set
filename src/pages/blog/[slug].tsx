b835609b48b44eaafacb94e13aed39685d90080a
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-06-17 23:09:57 -0300

    initial sharing component

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -7,6 +7,7 @@ import {
   BreadcrumbList,
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb'
+import { Button } from '@/components/ui/button'
 
 import { allPosts } from 'contentlayer/generated'
 import Image from 'next/image'
@@ -69,6 +70,21 @@ export default function PostPage() {
               <Markdown content={post?.body.raw} />
             </div>
           </article>
+
+          <aside className="space-y-6">
+            <div className="rounded-lg bg-gray-700 p-4 md:p-6">
+              <h2 className="mb-4 text-heading-xs text-gray-100">
+                Compartilhar
+              </h2>
+              <div className="space-y-3">
+                {[{ key: '1', providerName: 'Linkedin' }].map(provider => (
+                  <Button key={provider.key} variant="outline">
+                    {provider.providerName}
+                  </Button>
+                ))}
+              </div>
+            </div>
+          </aside>
         </div>
       </div>
     </main>
