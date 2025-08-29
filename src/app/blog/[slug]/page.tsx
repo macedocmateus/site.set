aa5bdb4c6e5969c94d02e221f602642d084500ee
mateus <mateusmacedoeng@gmail.com>
2025-08-29 16:36:34 -0300

    applying SSG and ISR in app router

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -8,6 +8,14 @@ type BlogPostPageParams = {
   }>
 }
 
+export const revalidate = 60
+
+export async function generateStaticParams() {
+  return allPosts.map(post => ({
+    slug: post.slug,
+  }))
+}
+
 export default async function BlogPostPage({ params }: BlogPostPageParams) {
   const { slug } = await params
   const post = allPosts.find(post => post.slug === slug)
